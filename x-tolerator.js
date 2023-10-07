e8716faee0b89ebacc64d832afae7a297cd32fd1
Stephen Chapman <dsasmblr@gmail.com>
2023-10-06 22:30:08 -0400

    Update x-tolerator.js
    Cleaned up the code a bit.

--- x-tolerator.js
+++ x-tolerator.js
@@ -6,23 +6,43 @@
 // @match        *://*.twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
+(() => {
     const srOnly = `
-    	position: absolute;
-        width: 1px;
-        height: 1px;
-        padding: 0;
-        margin: -1px;
-        overflow: hidden;
-        clip: rect(0, 0, 0, 0);
-        white-space: nowrap;
         border-width: 0;
-        pointer-events: none;
+        clip: rect(0, 0, 0, 0);
+        height: 1px;
+        margin: -1px;
         opacity: 0;
+        overflow: hidden;
+        padding: 0;
+        pointer-events: none;
+    	position: absolute;
+        white-space: nowrap;
+        width: 1px;
+    `;
+
+    const trendingMrow = `
+        <h2 style="
+            color: white;
+            font: 20px -apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Helvetica,Arial,sans-serif;
+            font-weight: 800;
+            padding-left: 16px;
+        ">
+            Trending Mrow!
+        </h2>
+        <div style="
+            align-items: center;
+            display: flex;
+            height: 205px;
+            justify-content: center;
+        ">
+            <a href="https://twitter.com/Chapman/status/1617296455584346113" style="padding-right: 15px;" target="_blank">
+                <img src="https://pbs.twimg.com/media/FnHK89PWQAABUOH.png" style="max-width: 100%;" />
+            </a>
+        </div>
     `;
 
     const HOME_PAGE = 'twitter.com/home';
@@ -30,30 +50,21 @@
 
     const setNahDog = () => {
         const isHomePage = location.href.includes(HOME_PAGE);
+        const forYouTab = document.querySelector(`${FOR_YOU_DIV} > div`);
         const forYouTabText = document.querySelector(`${FOR_YOU_DIV} > span`);
 
-        if (!forYouTabText) {
+        if (!isHomePage || !forYouTab || !forYouTabText) {
             return;
         }
 
-        const isForYouText = forYouTabText.innerText === 'For you';
+        const isForYouText = forYouTabText?.innerText === 'For you';
+        const forYouTabIsActive = forYouTab?.className.length > 30;
 
-        if (isHomePage && isForYouText) {
+        if (isForYouText) {
             forYouTabText.innerText = 'Nah, dog';
         }
-    }
 
-    const setFollowing = () => {
-        const isHomePage = location.href.includes(HOME_PAGE);
-        const forYouTab = document.querySelector(`${FOR_YOU_DIV} > div`);
-
-        if (!forYouTab) {
-            return;
-        }
-
-        const forYouTabIsActive = forYouTab.className.length > 30;
-
-        if (isHomePage && forYouTabIsActive) {
+        if (forYouTabIsActive) {
             const followingTab = [...document.querySelectorAll('[role=tab]')][1];
             followingTab.click();
             followingTab.focus();
@@ -65,49 +76,36 @@
             x.closest("[data-testid='cellInnerDiv']").style.cssText = srOnly;
         });
 
-        document.querySelector("[aria-label='Subscribe to Premium']").style.cssText = srOnly;
+        const premium = document.querySelector("[aria-label='Subscribe to Premium']");
+
+        if (premium) {
+            premium.style.cssText = srOnly;
+        }
     }
 
     const setMrow = () => {
         const trendingNow = document.querySelector('[aria-label="Timeline: Trending now"]');
 
         if (trendingNow) {
-          trendingNow.parentElement.innerHTML = `
-            <h2 style="
-              color: white;
-              font: 20px -apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,Helvetica,Arial,sans-serif;
-              font-weight: 800;
-              padding-left: 16px;
-            ">
-              Trending mrow
-            </h2>
-            <div style="
-              align-items: center;
-              display: flex;
-              height: 205px;
-              justify-content: center;
-            ">
-              <a href="https://twitter.com/Chapman/status/1617296455584346113" style="padding-right: 15px;" target="_blank">
-                <img src="https://pbs.twimg.com/media/FnHK89PWQAABUOH.png" style="max-width: 100%;" />
-              </a>
-            </div>
-          `;
+          trendingNow.parentElement.innerHTML = trendingMrow;
         }
     }
 
-    const t = document.body;
-    const c = { childList: true, subtree: true };
+    const target = document.body;
+    const config = {
+        childList: true,
+        subtree: true
+    }
 
-    const cb = (mutList, obs) => {
-        mutList.forEach((mut) => {
+    const callback = (mutList, obs) => {
+        if (mutList) {
             noAds();
             setMrow();
             setNahDog();
-            mut.type === 'childList' ? setFollowing() : null;
-        });
-    };
+        }
+    }
 
-    const obs = new MutationObserver(cb);
+    const obs = new MutationObserver(callback);
 
-    obs.observe(t, c);
+    obs.observe(target, config);
 })();
